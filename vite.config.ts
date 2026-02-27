7d91ce3e3504e2261d7168e69cd7f713a6792e47
山田秀和 <matunokihanten@gmail.com>
2026-02-27 20:08:20 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
-import pages from '@hono/vite-cloudflare-pages'
 
 export default defineConfig({
-  plugins: [
-    pages()
-  ],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    target: 'node22',
+    lib: {
+      entry: 'src/index.tsx',
+      formats: ['es'],
+      fileName: 'index'
+    },
+    rollupOptions: {
+      // バックエンド用のパッケージはビルドに含めないように設定します
+      external: [
+        'hono',
+        '@hono/node-server',
+        '@hono/node-server/serve-static',
+        'pg',
+        /^node:/ // Node.js標準モジュールをすべて除外
+      ]
+    }
   }
 })
